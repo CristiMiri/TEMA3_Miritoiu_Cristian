5c9a2d5a7ecc0fe53f60053be968e7a6040507c7
Miritoiu Cristian <miritoiu.george@yahoo.com>
2024-04-13 14:36:10 +0300

    setup

---
+++ Model/Repository/UtilizatorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS_TEMA3.Model.Repository
+{
+    internal class UtilizatorRepository
+    {
+    }
+}
